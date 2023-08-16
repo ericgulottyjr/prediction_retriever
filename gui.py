1da790eb93bbb586fa12770af9ee505fd96c6c29
Eric Gulotty <ericgulottyjr@gmail.com>
2023-08-16 15:06:50 -0400

    Add Calendar to gui

--- gui.py
+++ gui.py
@@ -1,11 +1,14 @@
 import tkinter as tk
+from tkinter import ttk
+from tkcalendar import Calendar
 from s3conn import download_files
+from datetime import datetime
 
 def start_download():
     result_label.config(text="Downloading...")
     download_files(
-        start_date_entry.get(),
-        end_date_entry.get(),
+        start_date_cal.get_date().strftime('%Y/%m/%d'),
+        end_date_cal.get_date().strftime('%Y/%m/%d'),
         minute_increment_entry.get(),
         seconds_var.get()
     )
@@ -14,25 +17,36 @@ def start_download():
 root = tk.Tk()
 root.title("Predictions Retriever")
 
-date_range_label = tk.Label(root, text="Date Range (YYYY/MM/DD - YYYY/MM/DD):")
+style = ttk.Style(root)
+style.theme_use('clam')
+
+date_range_label = tk.Label(root, text="Select Date Range:")
 date_range_label.pack()
 
-start_date_entry = tk.Entry(root)
-start_date_entry.pack()
+# Initialize the calendar to the current date
+current_date = datetime.now().date()
+start_date_cal = Calendar(root, selectmode='day', date_pattern='yyyy/mm/dd', 
+                          year=current_date.year, month=current_date.month, day=current_date.day,
+                          borderwidth=0.5, background='b'
+                          )
+start_date_cal.pack()
 
-end_date_entry = tk.Entry(root)
-end_date_entry.pack()
+end_date_cal = Calendar(root, selectmode='day', date_pattern='yyyy/mm/dd', 
+                        year=current_date.year, month=current_date.month, day=current_date.day,
+                        borderwidth=0.5, background='b'
+                        )
+end_date_cal.pack()
 
 minute_increment_label = tk.Label(root, text="Minute Increment:")
 minute_increment_label.pack()
 
-minute_increment_entry = tk.Entry(root)
+minute_increment_entry = tk.Entry(root, width=5)
 minute_increment_entry.pack()
 
 seconds_label = tk.Label(root, text="Second Increment:")
 seconds_label.pack()
 
-seconds_options = [str(i) for i in range(1, 61)]
+seconds_options = [str(i) for i in range(0, 61, 10)]
 seconds_var = tk.StringVar(value=seconds_options[0])
 seconds_menu = tk.OptionMenu(root, seconds_var, *seconds_options)
 seconds_menu.pack()
