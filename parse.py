de0c0480c3c45f78369101046d002c39760ab5b6
Eric Gulotty <ericgulottyjr@gmail.com>
2023-08-16 16:58:19 -0400

    Allow user input

--- parse.py
+++ parse.py
@@ -7,10 +7,14 @@ from datetime import datetime
 # Specify the base directory
 base_directory = 'temp'
 
+# Ask the user for a list of stop IDs
+stops = input("Enter a list of space separated stop IDs: ")
+stop_list = list(stops.split(" "))
+
 # Initialize the dataframes dictionary to store DataFrames for each date
 dataframes = {}
 
-# Step 2: Iterate through the year, month, and day directories under the temp directory
+# Iterate through the year, month, and day directories under the temp directory
 for year in os.listdir(base_directory):
     year_directory = os.path.join(base_directory, year)
     if os.path.isdir(year_directory):
@@ -20,7 +24,7 @@ for year in os.listdir(base_directory):
                 for day in os.listdir(month_directory):
                     day_directory = os.path.join(month_directory, day)
                     if os.path.isdir(day_directory):
-                        # Step 3: Iterate through .gz files in each date directory, extract JSON content, and parse it
+                        # Iterate through .gz files in each date directory, extract JSON content, and parse it
                         for root, dirs, files in os.walk(day_directory):
                             for file_name in files:
                                 if file_name.endswith('.gz'):
@@ -38,13 +42,13 @@ for year in os.listdir(base_directory):
                                                     for stop_update in stop_time_updates:
                                                         if 'departure' in stop_update and 'stop_id' in stop_update:
                                                             stop_id = stop_update['stop_id']
-                                                            if stop_id in ['70094', '70105', 'Braintree-01', 'Braintree-02',]:
+                                                            if stop_id in stop_list:
                                                                 departure_time = stop_update['departure']['time']
                                                                 current_time = file_name.split('_')[0].split('T')[1].replace('Z','').replace('/', ':')
                                                                 datetime_str = f"{year}-{month}-{day} {current_time}"
                                                                 datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
 
-                                                                # Step 5: Initialize DataFrames and append rows based on the extracted data
+                                                                # Initialize DataFrames and append rows based on the extracted data
                                                                 dataframe_name = f"{year}{month}{day}"
                                                                 if dataframe_name not in dataframes:
                                                                     dataframes[dataframe_name] = pd.DataFrame(columns=['id', 'stop_id', 'current_time', 'departure_time'])
@@ -57,7 +61,7 @@ for year in os.listdir(base_directory):
                                                                     ignore_index=True
                                                                 )
 
-# Step 6: Save each DataFrame as a CSV file
+# Save each DataFrame as a CSV file
 agg_csvs_directory = os.path.join(base_directory, 'agg_csvs')
 os.makedirs(agg_csvs_directory, exist_ok=True)
 for dataframe_name, df in dataframes.items():
